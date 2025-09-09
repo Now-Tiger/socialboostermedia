0aff97fd3d89a8d6b6118f6813f9a3d63dd48eaf
Swapnil Narwade <swapnil.narwade3@gmail.com>
2025-09-09 23:02:37 +0530

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  env: {
-    djangoBackendUrl: "http://52.91.1.148:8000/",
-    awsUserAnalyticsRoute: "http://52.91.1.148:8000/user-analytics/",
-    djangoBackendLocalhost: "http://localhost:8000/",
-    repo: "https://github.com/Now-Tiger/socialboostermedia",
-    publicBitCoinBaseUrl:
-      "https://data-api.coindesk.com/index/cc/v1/latest/tick",
-  },
-
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://52.91.1.148:8000/:path*", // only rewrite here
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/api/:path*", // only headers here
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        destination: "http://52.91.1.148:8000/:path*", // EC2 backend
       },
     ];
   },
